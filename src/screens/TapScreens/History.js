1d3e1970333798de8ddb456c37e9114e37e4d8b5
fordaf4445 <fordaf4445@gmail.com>
2022-12-20 18:47:09 +0700

    pass

--- src/screens/TapScreens/History.js
+++ src/screens/TapScreens/History.js
@@ -1,7 +1,38 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import auth, { firebase } from '@react-native-firebase/auth';
 
 const History = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [users, setUser] = useState([]);
+
+
+
+    useEffect(() => {
+        const getData = firebase.firestore();
+        const subscriber = getData.collection("dataUser")
+        .onSnapshot(querySnapshot => {
+            const users = [];
+
+            querySnapshot.forEach(documentSnapshot => {
+                users.push({
+                    ...documentSnapshot.data(),
+                    key: documentSnapshot.id,
+                });
+            });
+
+            setUser(users);
+            setLoading(false);
+        });
+
+    return () => subscriber();
+    },[]);
+
+    if (loading) {
+        return <ActivityIndicator />
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.titleBar}>
@@ -11,9 +42,14 @@ const History = () => {
 
             </View>
             <View style={styles.borderHistory}>
-                <ScrollView>
-
-                </ScrollView>
+                <FlatList
+                data={users}
+                renderItem={({item}) => (
+                    <View style={{height: 50, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                        <Text style={{color:"red"}}>User Email: {item.email}</Text>
+                        <Text>User Name: {item.username}</Text>
+                    </View>
+                )}/>
             </View>
         </View>
     )
@@ -35,13 +71,24 @@ const styles = StyleSheet.create({
         fontFamily: 'NotoSansThai-Bold',
         color: 'black',
     },
-    borderHistory:{
-        flex:1,
-        borderWidth:1,
-        borderRadius:15,
-        margin:20,
+    borderHistory: {
+        flex: 1,
+        borderWidth: 1,
+        borderRadius: 15,
+        margin: 20,
         borderColor: '#e8e8e8',
         backgroundColor: 'white',
-        
+
     },
-})
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+
+    },
+    title: {
+        fontSize: 32,
+    },
+
+});
