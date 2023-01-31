90d56e73304db1c4c46ac2a175b733d5049f2dec
fordaf4445 <fordaf4445@gmail.com>
2023-01-31 21:33:12 +0700

    profileUpdate 31/01/2023

--- src/screens/TapScreens/Profile.js
+++ src/screens/TapScreens/Profile.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, StyleSheet, TextInput, ScrollView, CheckBox, Image, TouchableOpacity, ActivityIndicator, Animated } from 'react-native';
-import { Button, Text, } from '@rneui/base';
-import Inicon from 'react-native-vector-icons/dist/Ionicons';
-import MaterialIcons from 'react-native-vector-icons/dist/MaterialIcons'
-import AlarmTitle from '../../components/ProfileComponents/AlarmTitle';
-import Savefile from '../../components/ProfileComponents/Savefile';
-import Title from '../../components/ProfileComponents/Title';
+import { View, StyleSheet, TextInput, ScrollView, CheckBox, Image, TouchableOpacity, ActivityIndicator, Animated, Text } from 'react-native';
+// import { Button, Text, } from '@rneui/base';
 import { AuthContext } from '../../auth/AuthProvider';
 import { firebase } from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
+import { NativeBaseProvider, HStack, VStack } from 'native-base';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Profile = () => {
 
@@ -44,70 +44,128 @@ const Profile = () => {
 
 
     return (
-        <View style={styles.container}>
-            <View style={{ backgroundColor: '#5DB075', flex: 1 }}>
-                <View style={[styles.titleBar]}>
-                    <TouchableOpacity onPress={() => { navigation.navigate('SettingComponent') }}>
-                        <Text style={styles.text}>ตั้งค่าผู้ใช้</Text>
-                    </TouchableOpacity>
-                    <Text style={styles.textHeader}>Profile</Text>
-                    <TouchableOpacity onPress={touchSignout}>
-                        <Text style={styles.text}>ออกระบบ</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{ alignSelf: "center", top: 20 }}>
-                    <View style={styles.profileImage}>
-                        <Image source={{uri : name.image}}
-                            style={styles.image} />
+        <NativeBaseProvider>
+            <VStack style={styles.container} alignItems="center" >
+                <VStack backgroundColor="#5DB075" width="100%" height="32%" borderBottomRadius={150} paddingTop={5}
+                    style={{
+                        shadowColor: "#000", elevation: 20,
+                    }}>
+                    <HStack justifyContent="space-around" >
+                        <TouchableOpacity onPress={() => { navigation.navigate('SettingComponent') }}>
+                            <Text style={styles.text}>ตั้งค่าผู้ใช้</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.textHeader}>Profile</Text>
+                        <TouchableOpacity onPress={touchSignout}>
+                            <Text style={styles.text}>ออกระบบ</Text>
+                        </TouchableOpacity>
+                    </HStack>
+                    <View style={{ alignSelf: "center", top: 35, }}>
+                        <View style={styles.profileImage}>
+                            <Image source={{ uri: name.image }}
+                                style={styles.image} />
+                        </View>
                     </View>
-                </View>
-            </View>
-            <View style={{ flex: 2 }}>
-                <View style={{ flex: 1, }}></View>
-                <View style={{ flex: 7, alignItems: 'center' }}>
+                </VStack>
+                <View style={{ width: "90%", height: "20%", marginTop: 65, alignItems: "center", }} >
                     <Text style={styles.textUsername} >{name.username}</Text>
-                    <Text style={{ fontSize: 16, fontFamily: 'NotoSansThai-Bold' }}>ข้อมูลส่วนตัว</Text>
-                    <Title />
-                    <Button
-                        title={<AlarmTitle />}
-                        icon={
-                            <Inicon name='alarm-outline' style={{ fontSize: 45, color: "black", marginRight: 65 }} />
-                        }
-                        buttonStyle={{
-                            borderBottomWidth: 1,
-                            borderColor: 'black',
-                            backgroundColor: ''
-                        }}
-                        containerStyle={{
-                            width: '90%',
-                            top: 20
-                        }}
-                        onPress={() => navigation.navigate('AlarmComponent')}
-                    />
-                    <Button
-                        title={<Savefile />}
-                        icon={{
-                            name: 'file-send-outline',
-                            type: 'material-community',
-                            size: 45,
+                    <VStack alignItems="flex-start" width="90%" space={2} padding={3} paddingLeft={7} borderRadius={30} backgroundColor="#fff"
+                        marginTop={1}
+                        style={{
+                            shadowColor: "#5DB075",
+                            elevation: 10,
+                        }}>
+                        <HStack space={3} alignItems="center">
+                            <HStack space={3} width="45%">
+                                <View style={styles.borderIconInfomation}>
+                                    <MaterialCommunityIcons
+                                        name="weight"
+                                        size={15}
+                                        color="#fff"
+                                    />
+                                </View>
+                                <Text style={styles.textInfomation}>น้ำหนัก  {name.weight}  กก.</Text>
+                            </HStack>
 
-                        }}
-                        iconContainerStyle={{ right: 40 }}
-                        buttonStyle={{
-                            borderBottomWidth: 1,
-                            borderColor: 'black',
-                            backgroundColor: ''
-                        }}
-                        containerStyle={{
-                            width: '90%',
-                            top: 20,
-                            // right: 10,
-
-                        }}
-                    />
+                            <View style={styles.borderIconInfomation}>
+                                <MaterialCommunityIcons
+                                    name="human-male-height"
+                                    size={15}
+                                    color="#fff"
+                                />
+                            </View>
+                            <Text style={styles.textInfomation}>ส่วนสูง  {name.hight}  ซม.</Text>
+                        </HStack>
+                        <HStack space={3} alignItems="center">
+                            <HStack space={3} width="45%">
+                                <View style={styles.borderIconInfomation}>
+                                    <MaterialIcons
+                                        name="data-usage"
+                                        size={15}
+                                        color="#fff"
+                                    />
+                                </View>
+                                <Text style={styles.textInfomation}>อายุ {name.age}  ปี</Text>
+                            </HStack>
+                            <View style={styles.borderIconInfomation}>
+                                <FontAwesome
+                                    name="intersex"
+                                    size={15}
+                                    color="#fff"
+                                />
+                            </View>
+                            <Text style={styles.textInfomation}>เพศ {name.sex}</Text>
+                        </HStack>
+                    </VStack>
                 </View>
-            </View>
-        </View>
+                <VStack width="97%" height="38%"  alignItems="center" backgroundColor="#fff" space={3}>
+                    <TouchableOpacity style={styles.layer} activeOpacity={0.9}
+                        onPress={() => navigation.navigate('AlarmComponent')}>
+                        <HStack alignItems="center" width="100%" height="100%" paddingLeft={2} space={5} >
+                            <View style={styles.borderIcon}>
+                               <Ionicons
+                               name='alarm'
+                               size={30}
+                               color="#5DB075"/>
+                            </View>
+                            <VStack >
+                                <Text style={styles.textTitle}>การแจ้งเตือน</Text>
+                                <Text style={styles.textLitle}>ตั้งการแจ้งเตือนและกำหนดการต่าง ๆ</Text>
+                            </VStack>
+                        </HStack>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.layer} activeOpacity={0.9}
+                        onPress={() => {}}>
+                        <HStack alignItems="center" width="100%" height="100%" paddingLeft={2} space={5} >
+                            <View style={styles.borderIcon}>
+                               <MaterialCommunityIcons
+                               name='file-move'
+                               size={30}
+                               color="#5DB075"/>
+                            </View>
+                            <VStack >
+                                <Text style={styles.textTitle}>บันทึกข้อมูลเป็นไฟล์เอกสาร</Text>
+                                <Text style={styles.textLitle}>บันทึกข้อมูลในฟอร์มเอกสารเพื่อส่งออก</Text>
+                            </VStack>
+                        </HStack>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.layer} activeOpacity={0.9}
+                        onPress={() => {}}>
+                        <HStack alignItems="center" width="100%" height="100%" paddingLeft={2} space={5} >
+                            <View style={styles.borderIcon}>
+                               <MaterialCommunityIcons
+                               name='help-circle'
+                               size={30}
+                               color="#5DB075"/>
+                            </View>
+                            <VStack >
+                                <Text style={styles.textTitle}>Help & About </Text>
+                                <Text style={styles.textLitle}>เกี่ยวกับแอพและช่วยเหลือ</Text>
+                            </VStack>
+                        </HStack>
+                    </TouchableOpacity>
+                </VStack>
+            </VStack>
+        </NativeBaseProvider>
     )
 }
 
@@ -116,10 +174,9 @@ export default Profile;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff',
     },
     titleBar: {
-        flexDirection: 'row',
-        justifyContent: "space-between",
         marginTop: 20,
         marginHorizontal: 16,
     },
@@ -130,39 +187,87 @@ const styles = StyleSheet.create({
         borderRadius: 100,
     },
     profileImage: {
-        width: 200,
-        height: 200,
+        width: 175,
+        height: 175,
         borderRadius: 100,
-        borderWidth: 5,
-        borderColor: 'white',
-        backgroundColor:"#fff"
-    },
-    add: {
-        backgroundColor: "#838383",
-        position: "absolute",
-        bottom: 10,
-        right: 10,
-        width: 40,
-        height: 40,
-        borderRadius: 30,
-        justifyContent: "center",
-        alignItems: "center",
-        // borderWidth:1,
+        borderWidth: 2,
+        borderColor: '#fff',
+        backgroundColor: "#fff",
+        shadowColor: "#000",
+        elevation: 20,
     },
     textHeader: {
         fontSize: 35,
         fontFamily: 'NotoSansThai-Bold',
-        color: 'white',
+        color: '#fff',
     },
     text: {
         fontSize: 16,
         fontFamily: 'NotoSansThai-Bold',
-        color: 'white',
-        top: 20,
+        color: '#fff',
+        marginTop: 20,
+    },
+    textTitle: {
+        fontSize: 19,
+        fontFamily: 'NotoSansThai-Bold',
+        color: '#fff',
+    },
+    textLitle: {
+        fontFamily: 'NotoSansThai-Regular',
+        color: '#fff',
     },
     textUsername: {
-        fontSize: 30,
+        fontSize: 26,
         fontFamily: 'NotoSansThai-Bold',
-        color: 'black',
+        color: '#000',
+    },
+    borderDetail: {
+        // borderWidth: 1,
+        width: "95%",
+        height: "62%",
+        marginTop: 20,
+        // borderRadius: 15,
+        // borderColor: '#e8e8e8',
+        // backgroundColor: 'white',
+        // shadowColor: "#000",
+        // elevation: 4,
+    },
+    layer: {
+        // borderWidth: 1,
+        width: "90%",
+        height: "28%",
+        borderRadius: 30,
+        // borderColor: '#e8e8e8',
+        backgroundColor: '#5db075',
+        shadowColor: "#000",
+        elevation: 10,
+        paddingLeft: 10,
+    },
+    borderIcon: {
+        // borderWidth:1,
+        width: 55,
+        height: 55,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 15,
+        shadowColor: "#000",
+        elevation: 10,
+        backgroundColor: '#fff',
+    },
+    borderIconInfomation: {
+        // borderWidth:1,
+        width: 20,
+        height: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 5,
+        shadowColor: "#000",
+        elevation: 5,
+        backgroundColor: '#5DB075',
+    },
+    textInfomation: {
+        fontFamily: 'NotoSansThai-Regular',
+        color: '#000',
+        fontSize: 14,
     },
 });
