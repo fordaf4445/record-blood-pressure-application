40ee9bbdf06a66c7beee17c85fdf2fb80e3b3e2b
fordaf4445 <fordaf4445@gmail.com>
2023-02-03 22:11:01 +0700

    03/02/2023

--- src/screens/TapScreens/Prosess.js
+++ src/screens/TapScreens/Prosess.js
@@ -1,32 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Animated, Text, TouchableOpacity, Button } from 'react-native';
-import {  FAB, Overlay  } from '@rneui/base';
+import { FAB, Overlay } from '@rneui/base';
 import { useNavigation } from '@react-navigation/native';
-import { GestureHandlerRootView, Swipeable } from 'react-native-gesture-handler';
 import FontAwesome5 from 'react-native-vector-icons/dist/FontAwesome5';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Entypo from 'react-native-vector-icons/Entypo';
-import { VStack, HStack, NativeBaseProvider } from 'native-base';
-
+import { VStack, HStack, NativeBaseProvider, Link } from 'native-base';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 const Prosess = () => {
     const [changeMode, setChangeMode] = useState(true);
-    const [openOverlayJNC7 , setOpenOverlayJNC7] = useState(false);
+    const [openOverlayJNC7, setOpenOverlayJNC7] = useState(false);
 
     const navigation = useNavigation();
+    const [openImage, setOpenImage] = useState(false);
+
+    const images = [{
+        props: {
+            source: require('../../../assets/image/ClassificationBloodpressure.png')
+        }
+    }]
 
     return (
         <NativeBaseProvider>
             <VStack style={styles.container} space={5} backgroundColor={changeMode == true ? ("#fff") : ("#5DB075")}>
-                <Overlay isVisible={openOverlayJNC7} overlayStyle={{backgroundColor:"#fff",height:"90%" ,width:"98%"}}>
-                    <View style={{}}>
-                        <Image
-                        source={require("../../../assets/image/ClassificationBloodpressure.png")}
-                        style={{height:330 ,width:"100%"}}/>
-                        <Button
-                        title='Close'
-                        onPress={() =>{setOpenOverlayJNC7(!openOverlayJNC7)}}/>
-                    </View>
+                <Overlay isVisible={openOverlayJNC7} onBackdropPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }} overlayStyle={{ backgroundColor: "#fff", height: "70%", width: "98%" }}>
+                    <Overlay isVisible={openImage} onBackdropPress={() => { setOpenImage(!openImage) }}
+                        overlayStyle={{ backgroundColor: "#fff", height: "60%", width: "98%", }}>
+                        <ImageViewer
+                            imageUrls={images}
+                            backgroundColor={"#fff"}
+                        />
+                        <HStack position="absolute" width="100%" height="90%" justifyContent={"flex-end"} paddingTop={3}>
+                            <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
+                                <MaterialCommunityIcons
+                                    name='close-circle'
+                                    size={30} />
+                            </TouchableOpacity>
+                        </HStack>
+                    </Overlay>
+                    <VStack>
+                        <HStack justifyContent={"flex-end"}>
+                            <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
+                                <MaterialCommunityIcons
+                                    name='close-circle'
+                                    size={30} />
+                            </TouchableOpacity>
+                        </HStack>
+                        <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
+                            <Image
+                                source={require('../../../assets/image/ClassificationBloodpressure.png')}
+                                style={{ width: "100%", height: 350 }}
+                                resizeMode='stretch'
+                            />
+                        </TouchableOpacity>
+                        <VStack padding={2.5}>
+                            <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#000", }}>
+                                อ้างอิงจาก หนังสือคู่มือการให้ความรู้ เพื่อจัดการภาวะความดันโลหิตสูงด้วยตนเอง หน้าที่ 16.
+                            </Text>
+                            <Link href="http://www.imrta.dms.moph.go.th/imrta/images/data/ht20121024.pdf" >
+                               <Text style={{fontFamily: "NotoSansThai-Regular", color: "#5DB075",fontSize:16, borderBottomWidth:1,borderBottomColor:"#5DB075" }}>
+                                อ่านเพิ่มเติมได้ที่นี่
+                                </Text> 
+                            </Link>
+                        </VStack>
+                    </VStack>
                 </Overlay>
                 <VStack style={styles.inputDataBluetooth}>
                 </VStack>
@@ -35,7 +74,7 @@ const Prosess = () => {
                         <Text style={{ fontFamily: "NotoSansThai-Regular", color: changeMode == true ? ("#000") : ("#fff") }}>
                             การแบ่งระดับความดันโลหิตสูงตามวิธี JNC 7
                         </Text>
-                        <TouchableOpacity onPress={() => {setOpenOverlayJNC7(!openOverlayJNC7)}}>
+                        <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
                             <Entypo name='help-with-circle' size={18} color={changeMode == true ? ("#5DB075") : ("#fff")} />
                         </TouchableOpacity>
                     </HStack>
