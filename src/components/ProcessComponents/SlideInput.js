1fba0353c2a2d258e8f9d559f61b7e4ac13f5b57
fordaf4445 <fordaf4445@gmail.com>
2023-02-07 21:48:59 +0700

    07/02/2023

---
+++ src/components/ProcessComponents/SlideInput.js
@@ -0,0 +1,208 @@
+import {
+    View, Text, Button, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard,
+    useWindowDimensions, TouchableOpacity, Alert
+} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { firebase } from '@react-native-firebase/auth';
+import moment from 'moment';
+import { VStack, HStack, NativeBaseProvider, Link } from 'native-base';
+
+const SlideInput = (
+    setVisible
+) => {
+    const { width: windowWidth } = useWindowDimensions();
+    const db = firebase.firestore();
+    const [sys, setSys] = useState('');
+    const [dia, setDia] = useState('');
+    const [bpm, setBpm] = useState('');
+
+    const dateToTime = current => {
+        return moment(current).format('L LT');
+    };
+
+    async function addInformationFireStore() {
+        await setVisible(true)
+        let type;
+        if (sys > 160) {
+            type = "ความดันโลหิตสูง ระยะที่ 2"
+        } else if (sys >= 141) {
+            type = "ความดันโลหิตสูง ระยะที่ 1"
+        } else if (sys >= 121) {
+            type = "ความดันโลหิตสูงขั้นต้น"
+        } else if (sys >= 91) {
+            type = "ปกติ"
+        } else {
+            type = "ความดันโลหิตต่ำ"
+        }
+
+        await db.collection('dataUser')
+            .doc(firebase.auth().currentUser.uid)
+            .collection('BloodPressure')
+            .add({
+                SYS: sys,
+                DIA: dia,
+                BPM: bpm,
+                timestamp: Date.now(),
+                TYPE: type,
+                TIME: dateToTime(Date.now()),
+            })
+            .then(function (docRef) {
+                setVisible(false)
+                console.log("Document written with ID: ", docRef.id);
+                // Alert.alert("เพิ่มข้อมูลสำเร็จ")
+                Alert.alert(
+                    "เพิ่มข้อมูลสำเร็จ",
+                    "",
+                    [
+                        { text: "OK", onPress: () => { setSys(''), setDia(''), setBpm('') } }
+                    ]
+                );
+
+            })
+            .catch(function (err) {
+                console.log("Error adding information: ", err);
+                Alert.alert("เพิ่มข้อมูลไม่สำเร็จ !!" + err.message)
+            })
+        { type }
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <HStack space={50} justifyContent={"center"} alignItems={"center"} >
+                {/* InputData form BlueTooth */}
+                <VStack key={1} style={[styles.container, { width: windowWidth }]}>
+                    <VStack style={styles.inPutContainer}><Text>SlideInputBlueTooth</Text></VStack>
+                </VStack>
+
+                {/* InputDataManually */}
+                <VStack key={2} style={[styles.container, { width: windowWidth }]}>
+                    <HStack style={styles.inPutContainer}>
+                        <VStack width={"80%"} padding={2} space={3} justifyContent={"space-around"}>
+                            <HStack style={styles.inPutDataManualLayer} space={3} >
+                                <VStack style={styles.inputLayerFront} backgroundColor={"#ff0000"} borderRadius={15}>
+                                    <Text style={{ fontFamily: "NotoSansThai-Bold", fontSize: 18, color: "#fff" }}>
+                                        SYS
+                                    </Text>
+                                    <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#fff" }}>
+                                        mmgh
+                                    </Text>
+                                </VStack>
+                                <TextInput style={[styles.inputNumLayer, { borderColor: "#ff0000", color: "#ff0000" }]}
+                                    keyboardType='numeric'
+                                    onChangeText={(sys) => setSys(sys)}
+                                    value={sys}
+                                    maxLength={3}
+                                />
+                            </HStack>
+
+                            <HStack style={styles.inPutDataManualLayer} space={3} >
+                                <VStack style={styles.inputLayerFront} backgroundColor={"#A4BF43"} borderRadius={15}>
+                                    <Text style={{ fontFamily: "NotoSansThai-Bold", fontSize: 18, color: "#fff" }}>
+                                        DIA
+                                    </Text>
+                                    <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#fff" }}>
+                                        mmgh
+                                    </Text>
+                                </VStack>
+                                <TextInput style={[styles.inputNumLayer, { borderColor: "#A4BF43", color: "#A4BF43" }]}
+                                    keyboardType='numeric'
+                                    onChangeText={(dia) => setDia(dia)}
+                                    value={dia}
+                                    maxLength={3}
+                                />
+                            </HStack>
+
+                            <HStack style={styles.inPutDataManualLayer} space={3} >
+                                <VStack style={styles.inputLayerFront} backgroundColor={"#23AFD6"} borderRadius={15}>
+                                    <Text style={{ fontFamily: "NotoSansThai-Bold", fontSize: 15, color: "#fff" }}>
+                                        PULSE
+                                    </Text>
+                                    <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#fff" }}>
+                                        bpm
+                                    </Text>
+                                </VStack>
+                                <TextInput style={[styles.inputNumLayer, { borderColor: "#23AFD6", color: "#23AFD6" }]}
+                                    keyboardType='numeric'
+                                    onChangeText={(bpm) => setBpm(bpm)}
+                                    value={bpm}
+                                    maxLength={3}
+                                />
+                            </HStack>
+
+                        </VStack>
+                        <TouchableOpacity style={styles.pushContainer}
+                            onPress={() => {
+                                if (sys == '' || dia == '' || bpm == '') {
+                                    Alert.alert('ไม่สารมารถเพิ่มข้อมูลได้', 'ไม่มีข้อมูลใน SYS,DIA หรือ BPM')
+                                } else { addInformationFireStore() }
+                            }}>
+                            <VStack>
+                                <Text style={{ fontFamily: "NotoSansThai-Bold", fontSize: 18, color: "#fff" }}>
+                                    เพิ่ม
+                                </Text>
+                            </VStack>
+                        </TouchableOpacity>
+                    </HStack>
+                </VStack>
+
+            </HStack>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default SlideInput;
+
+const styles = StyleSheet.create({
+    container: {
+        height: "90%",
+        padding: 15,
+    },
+    inPutContainer: {
+        borderRadius: 35,
+        padding: 15,
+        height: "100%",
+        width: "100%",
+        shadowColor: "#000",
+        elevation: 10,
+        backgroundColor: "#fff",
+    },
+    inPutDataManualLayer: {
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 70,
+        backgroundColor: "white",
+        shadowColor: "#000",
+        elevation: 10,
+    },
+    pushContainer: {
+        // borderWidth: 1,
+        heigh: "100%",
+        width: "20%",
+        borderRadius: 25,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5DB075",
+        shadowColor: "#000",
+        elevation: 10,
+    },
+    inputNumLayer: {
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        height: 50,
+        borderWidth: 1,
+        paddingLeft: 20,
+        width: 150,
+        fontSize: 25,
+        shadowColor: "#000",
+        elevation: 5,
+    },
+    inputLayerFront: {
+        width: "25%",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 50,
+        shadowColor: "#000",
+        elevation: 10,
+    },
+});
--- src/screens/TapScreens/Prosess.js
+++ src/screens/TapScreens/Prosess.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Image, Animated, Text, TouchableOpacity, Button } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, StyleSheet, Image, Animated, Text, TouchableOpacity, Button, ScrollView, useWindowDimensions } from 'react-native';
 import { FAB, Overlay } from '@rneui/base';
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome5 from 'react-native-vector-icons/dist/FontAwesome5';
@@ -9,12 +9,17 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import { VStack, HStack, NativeBaseProvider, Link } from 'native-base';
 import ImageViewer from 'react-native-image-zoom-viewer';
 
-const Prosess = () => {
-    const [changeMode, setChangeMode] = useState(true);
-    const [openOverlayJNC7, setOpenOverlayJNC7] = useState(false);
+import SlideInput from '../../components/ProcessComponents/SlideInput';
 
+
+const Prosess = () => {
+    const [openOverlayJNC7, setOpenOverlayJNC7] = useState(false);
     const navigation = useNavigation();
     const [openImage, setOpenImage] = useState(false);
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const { width: windowWidth } = useWindowDimensions();
+    const [dotIndicator, setDotIndicator] = useState(0);
+    const [visible, setVisible] = useState(false);
 
     const images = [{
         props: {
@@ -23,97 +28,132 @@ const Prosess = () => {
     }]
 
     return (
+
         <NativeBaseProvider>
-            <VStack style={styles.container} space={5} backgroundColor={changeMode == true ? ("#fff") : ("#5DB075")}>
-                <Overlay isVisible={openOverlayJNC7} onBackdropPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }} overlayStyle={{ backgroundColor: "#fff", height: "70%", width: "98%" }}>
-                    <Overlay isVisible={openImage} onBackdropPress={() => { setOpenImage(!openImage) }}
-                        overlayStyle={{ backgroundColor: "#fff", height: "60%", width: "98%", }}>
-                        <ImageViewer
-                            imageUrls={images}
-                            backgroundColor={"#fff"}
-                        />
-                        <HStack position="absolute" width="100%" height="90%" justifyContent={"flex-end"} paddingTop={3}>
-                            <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
-                                <MaterialCommunityIcons
-                                    name='close-circle'
-                                    size={30} />
-                            </TouchableOpacity>
-                        </HStack>
+            <ScrollView style={{ flex: 1, backgroundColor: "#fff" }} >
+                <VStack style={styles.container} space={5} backgroundColor={"#fff"}>
+
+                    <Overlay isVisible={visible} overlayStyle={{ borderColor: "red", borderRadius: 25, backgroundColor: "#fff" }}>
+                        <View style={{ alignItems: "center", width: 150 }}>
+                            <Animated.Image
+                                source={require("../../../assets/gif/heartLoading.gif")}
+                                style={{ width: 70, height: 70 }}
+                                resizeMode='cover' />
+                            {/* <ActivityIndicator size='large' /> */}
+                            <Text style={{ fontFamily: "NotoSansThai-Bold", fontSize: 18, color: "#000" }}>กรุณารอสักครู่..</Text>
+                        </View>
                     </Overlay>
-                    <VStack>
-                        <HStack justifyContent={"flex-end"}>
-                            <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
-                                <MaterialCommunityIcons
-                                    name='close-circle'
-                                    size={30} />
-                            </TouchableOpacity>
-                        </HStack>
-                        <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
-                            <Image
-                                source={require('../../../assets/image/ClassificationBloodpressure.png')}
-                                style={{ width: "100%", height: 350 }}
-                                resizeMode='stretch'
+
+                    <Overlay isVisible={openOverlayJNC7} onBackdropPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }} overlayStyle={{ backgroundColor: "#fff", height: "70%", width: "98%" }}>
+                        <Overlay isVisible={openImage} onBackdropPress={() => { setOpenImage(!openImage) }}
+                            overlayStyle={{ backgroundColor: "#fff", height: "60%", width: "98%", }}>
+                            <ImageViewer
+                                imageUrls={images}
+                                backgroundColor={"#fff"}
                             />
-                        </TouchableOpacity>
-                        <VStack padding={2.5}>
-                            <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#000", }}>
-                                อ้างอิงจาก หนังสือคู่มือการให้ความรู้ เพื่อจัดการภาวะความดันโลหิตสูงด้วยตนเอง หน้าที่ 16.
-                            </Text>
-                            <Link href="http://www.imrta.dms.moph.go.th/imrta/images/data/ht20121024.pdf" >
-                               <Text style={{fontFamily: "NotoSansThai-Regular", color: "#5DB075",fontSize:16, borderBottomWidth:1,borderBottomColor:"#5DB075" }}>
-                                อ่านเพิ่มเติมได้ที่นี่
-                                </Text> 
-                            </Link>
+                            <HStack position="absolute" width="100%" height="90%" justifyContent={"flex-end"} paddingTop={3}>
+                                <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
+                                    <MaterialCommunityIcons
+                                        name='close-circle'
+                                        size={30} />
+                                </TouchableOpacity>
+                            </HStack>
+                        </Overlay>
+                        <VStack>
+                            <HStack justifyContent={"flex-end"}>
+                                <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
+                                    <MaterialCommunityIcons
+                                        name='close-circle'
+                                        size={30} />
+                                </TouchableOpacity>
+                            </HStack>
+                            <TouchableOpacity onPress={() => { setOpenImage(!openImage) }}>
+                                <Image
+                                    source={require('../../../assets/image/ClassificationBloodpressure.png')}
+                                    style={{ width: "100%", height: 350 }}
+                                    resizeMode='stretch'
+                                />
+                            </TouchableOpacity>
+                            <VStack padding={2.5}>
+                                <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#000", }}>
+                                    อ้างอิงจาก หนังสือคู่มือการให้ความรู้ เพื่อจัดการภาวะความดันโลหิตสูงด้วยตนเอง หน้าที่ 16.
+                                </Text>
+                                <Link href="http://www.imrta.dms.moph.go.th/imrta/images/data/ht20121024.pdf" >
+                                    <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#5DB075", fontSize: 16, borderBottomWidth: 1, borderBottomColor: "#5DB075" }}>
+                                        อ่านเพิ่มเติมได้ที่นี่
+                                    </Text>
+                                </Link>
+                            </VStack>
+                        </VStack>
+                    </Overlay>
+
+                    <VStack style={styles.inputDataBluetooth} alignItems={"center"} >
+                        <ScrollView
+                            horizontal={true}
+                            decelerationRate={"fast"}
+                            snapToInterval={1000}
+                            showsHorizontalScrollIndicator={false}
+                            // bounces={false}
+                            // scrollEventThrottle={16}
+                            // pagingEnabled
+                            onScroll={
+                                e => { setDotIndicator((e.nativeEvent.contentOffset.x).toFixed(2)) }
+                            }
+                        >
+                            {SlideInput(setVisible)}
+                        </ScrollView>
+                        <VStack style={styles.indicatorContainer}>
+                            <HStack style={[styles.normalDot, { width: dotIndicator <= 230 ? (16) : (8) }]} />
+                            <HStack style={[styles.normalDot, { width: dotIndicator >= 230 ? (16) : (8) }]} />
                         </VStack>
                     </VStack>
-                </Overlay>
-                <VStack style={styles.inputDataBluetooth}>
+
+                    <VStack height="35%" width="100%" space={1.5} alignItems="center" >
+                        <HStack alignItems="center" space={2} >
+                            <Text style={{ fontFamily: "NotoSansThai-Regular", color: "#000", }}>
+                                การแบ่งระดับความดันโลหิตในผู้ใหญ่
+                            </Text>
+                            <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
+                                <Entypo name='help-with-circle' size={18} color={"#5DB075"} />
+                            </TouchableOpacity>
+                        </HStack>
+                        <VStack style={styles.inforMation} space={2}>
+                            <HStack style={styles.inforMationLayer} alignItems={"center"} backgroundColor="white">
+                                <HStack width={"55%"} justifyContent={"center"}><Text style={styles.inforMationtext}>ประเภท</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>SYS</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>DIA</Text></HStack>
+                            </HStack>
+                            <HStack style={styles.inforMationLayer} backgroundColor="#EF553C" alignItems={"center"}>
+                                <HStack width={"55%"}><Text style={styles.inforMationtext}>ความดันโลหิตสูง ระยะที่ 2</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>&gt; 160</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>&gt; 100</Text></HStack>
+
+                            </HStack>
+                            <HStack style={styles.inforMationLayer} backgroundColor="#F1815C" alignItems={"center"}>
+                                <HStack width={"55%"} ><Text style={styles.inforMationtext}>ความดันโลหิตสูง ระยะที่ 1</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>141-160</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>91-100</Text></HStack>
+                            </HStack>
+                            <HStack style={styles.inforMationLayer} backgroundColor="#EEC151" alignItems={"center"}>
+                                <HStack width={"55%"} ><Text style={styles.inforMationtext}>ความดันโลหิตสูงขั้นต้น</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>121-140</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>81-90</Text></HStack>
+                            </HStack>
+                            <HStack style={styles.inforMationLayer} backgroundColor="#B8DE9A" alignItems={"center"}>
+                                <HStack width={"55%"} ><Text style={styles.inforMationtext}>ปกติ</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>91-120</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>61-80</Text></HStack>
+                            </HStack>
+                            <HStack style={styles.inforMationLayer} backgroundColor="#71C7E2" alignItems={"center"}>
+                                <HStack width={"55%"} ><Text style={styles.inforMationtext}>ความดันโลหิตต่ำ</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>&lt; 90</Text></HStack>
+                                <HStack width={"22.5%"} justifyContent={"center"}><Text style={styles.inforMationtext}>&lt; 60</Text></HStack>
+                            </HStack>
+                        </VStack>
+                    </VStack >
+
                 </VStack>
-                <VStack height="37%" width="100%" borderWidth={1} space={1.5} alignItems="center">
-                    <HStack alignItems="center" space={2} >
-                        <Text style={{ fontFamily: "NotoSansThai-Regular", color: changeMode == true ? ("#000") : ("#fff") }}>
-                            การแบ่งระดับความดันโลหิตสูงตามวิธี JNC 7
-                        </Text>
-                        <TouchableOpacity onPress={() => { setOpenOverlayJNC7(!openOverlayJNC7) }}>
-                            <Entypo name='help-with-circle' size={18} color={changeMode == true ? ("#5DB075") : ("#fff")} />
-                        </TouchableOpacity>
-                    </HStack>
-                    <VStack style={styles.inforMation} space={2}>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ประเภท  SYS  DIA</Text>
-                        </HStack>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ความดันโลหิตสูง ระยะที่ 2</Text>
-                        </HStack>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ความดันโลหิตสูง ระยะที่ 1</Text>
-                        </HStack>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ความดันโลหิตสูงขั้นต้น</Text>
-                        </HStack>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ปกติ</Text>
-                        </HStack>
-                        <HStack style={styles.inforMationLayer}>
-                            <Text>ความดันโลหิตต่ำ</Text>
-                        </HStack>
-                    </VStack>
-                </VStack >
-                <VStack style={styles.fabContainer} alignItems="flex-end" justifyContent="flex-end">
-                    <HStack style={styles.inFabContainer} space={2}>
-                        <Text>change mode Input</Text>
-                        <FAB
-                            onPress={() => { setChangeMode(!changeMode) }}
-                            icon={
-                                <FontAwesome
-                                    name='refresh' size={25} color={changeMode == true ? ("#fff") : ("#5DB075")}
-                                />}
-                            color={changeMode == true ? ("#5DB075") : ("#fff")}
-                            style={{}}
-                        />
-                    </HStack>
-                </VStack>
-            </VStack>
+            </ScrollView>
         </NativeBaseProvider>
     )
 }
@@ -124,43 +164,48 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         alignItems: 'center',
-        borderWidth: 1,
-        height: "100%",
+        height: 700,
     },
     inputDataBluetooth: {
-        borderWidth: 1,
-        width: "100%",
-        height: "30%",
-        borderRadius: 35,
+        width: "111%",
+        height: "50%",
+        backgroundColor: "white",
     },
     inforMation: {
-        borderWidth: 1,
         width: "100%",
         // height: "100%",
         borderRadius: 35,
         padding: 10,
+        paddingTop: 5,
         alignItems: "center",
         justifyContent: "center",
+        shadowColor: "#000",
+        elevation: 10,
+        backgroundColor: "#fff",
     },
     inforMationLayer: {
-        borderWidth: 1,
         width: "100%",
-        height: "11%",
+        height: "12%",
+        borderRadius: 25,
+        paddingLeft: 15,
+        shadowColor: "#000",
+        elevation: 5,
     },
-    fabContainer: {
-        width: "100%",
-        height: "25%",
-        borderBottomColor: "red",
-        borderWidth: 1,
+    inforMationtext: {
+        color: '#000',
+        fontFamily: 'NotoSansThai-Regular',
     },
-    inFabContainer: {
-        alignItems: "center",
-    },
-    text: {
-        color: '#5DB075',
-        top: 10,
-        fontFamily: 'NotoSansThai-SemiBold',
-        backgroundColor: "#f2f2f2",
-    },
+    indicatorContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
 
+    },
+    normalDot: {
+        height: 8,
+        width: 8,
+        borderRadius: 4,
+        backgroundColor: '#5DB075',
+        marginHorizontal: 4,
+    },
 });
