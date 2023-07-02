b2632a662e13af5624909ee872a0d66e9f311856
fordaf4445 <fordaf4445@gmail.com>
2023-07-02 19:14:08 +0700

    update 63 moderate

--- src/components/ProfileComponents/SaveComponent/SaveComponent.js
+++ src/components/ProfileComponents/SaveComponent/SaveComponent.js
@@ -1,22 +1,58 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { printToFileAsync } from 'expo-print';
+import { shareAsync } from 'expo-sharing';
+
 
 const SaveComponent = () => {
-  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+  let [name, setName] = useState("");
+
+  const html = `
+    <html>
+      <body>
+        <h1>Hi </h1>
+        <p style="color: red;">Hello. Bonjour. Hola.</p>
+      </body>
+    </html>
+  `;
+
+  let generatePDF = async () => {
+    const file = await printToFileAsync({
+      html: html,
+      base64: false
+    });
+
+    await shareAsync(file.uri);
+    
+  };
+
   return (
-    <View>
-      <Text>SaveComponent good morning</Text>
+    <View style={styles.container}>
       <TextInput
-
-        multiline
-        numberOfLines={4}
-
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        style={{ padding: 10 , borderWidth : 1}}
-      />
+      value={name}
+      placeholder='Name'
+      style={styles.inputTextName}
+      onChangeText={(value) => setName(value)}/>
+      <Button
+       title='GeneratePDF'
+       onPress={generatePDF}/>
     </View>
-  )
+  );
 }
 
 export default SaveComponent
+
+const styles = StyleSheet.create ({
+    container : {
+      flex : 1,
+      backgroundColor : "#fff",
+      justifyContent : "center",
+      alignItems : "center",
+    },
+    inputTextName : {
+      borderWidth : 1,
+      borderRadius : 10,
+      width: 200,
+      marginBottom : 20,
+    },
+});
